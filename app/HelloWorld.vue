289cf01213860b16de4f1ceef2257b1abdfb9b3d
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-01-06 23:03:01 +0100

    Refactor HelloWorld.vue to use script export

--- app/HelloWorld.vue
+++ app/HelloWorld.vue
@@ -1,14 +1,18 @@
-<script setup>
+<script>
 import { version as vueVersion } from 'vue'
 import { computed } from 'vue'
 import { useRoute } from 'vue-router'
 
-const route = useRoute()
-const name = computed(() => route.query.name ?? route.params.name ?? 'World')
+export default {
+  setup() {
+    const route = useRoute()
+    const name = computed(() => route.query.name ?? route.params.name ?? 'World')
+    return { name, vueVersion }
+  }
+}
 </script>
 
 <template>
-  <main style="padding:1rem; font-family:system-ui,sans-serif">
-    Hello {{ name }}! (Vue {{ vueVersion }})
-  </main>
+  <h1>Hello {{ name }}!</h1>
+  <h5>(Vue {{ vueVersion }})</h5>
 </template>
