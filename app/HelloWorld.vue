1b53df63cadf82b5fd75b644db45c7be6af85706
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-01-06 09:38:14 +0100

    Add HelloWorld component with props and template

---
+++ app/HelloWorld.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { version as vieVersion } from 'vue'
+import { defineProps } from 'vue'
+
+defineProps {(
+  name: { type: String, default 'World' }
+)}
+</script>
+
+<template>
+  <main style="padding:1rem; font-family:system-ui,sans-serif">
+    Hello {{ name }}! (Vue {{ vueVersion }})
+  </main>
+</template>
