8c82fec92a59e62ea65b820902a4b50c272e904f
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-01-06 23:28:28 +0100

    Update podman run command to change port mapping

--- podman-run-vue.bash
+++ podman-run-vue.bash
@@ -1,3 +1,4 @@
 podman build --no-cache --rm -f Containerfile -t vue:demo .
-podman run --interactive --tty -p 5173:8080 vue:demo
+#podman run --interactive --tty -p 5173:8080 vue:demo
+podman run --interactive --tty -p 5173:80 vue:demo
 echo "browse http://localhost:5173/"
