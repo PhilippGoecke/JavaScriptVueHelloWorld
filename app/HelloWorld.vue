e3ef39878b3a00af1bdd7e15c8716832df0121ad
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-01-07 12:08:11 +0100

    Fix optional chaining in console logs and watchers

--- app/HelloWorld.vue
+++ app/HelloWorld.vue
@@ -7,12 +7,12 @@ const route = useRoute()
 const name = computed(() => route.query?.name?.trim() || route.params?.name?.trim() || 'World')
 
 watchEffect(() => {
-  console.log('route.query:', route.query)
-  console.log('route.params:', route.params)
+  console.log('route.query:', route.query?)
+  console.log('route.params:', route.params?)
   console.log('name:', name.value)
 })
 
-watch(() => [route.query?.name, route.params?.name], ([q, p]) => {
+watch(() => [route.query?.name?, route.params?.name?], ([q, p]) => {
   console.log('query.name:', q, 'params.name:', p, 'computed name:', name.value)
 })
 </script>
