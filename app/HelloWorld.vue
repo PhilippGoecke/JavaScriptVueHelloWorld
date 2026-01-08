1b8a50b03f1ad0bb54d1b61de1d7688b0f6cc011
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-01-08 21:11:37 +0100

    Simplify name computed property in HelloWorld.vue
    Refactor name computed property for better safety.

--- app/HelloWorld.vue
+++ app/HelloWorld.vue
@@ -1,20 +1,10 @@
 <script setup>
 import { version as vueVersion } from 'vue'
-import { computed, watchEffect, watch } from 'vue'
+import { computed } from 'vue'
 import { useRoute } from 'vue-router'
 
 const route = useRoute()
-const name = computed(() => route.query?.name?.trim() || route.params?.name?.trim() || 'World')
-
-watchEffect(() => {
-  console.log('route.query:', route.query?)
-  console.log('route.params:', route.params?)
-  console.log('name:', name.value)
-})
-
-watch(() => [route.query?.name?, route.params?.name?], ([q, p]) => {
-  console.log('query.name:', q, 'params.name:', p, 'computed name:', name.value)
-})
+const name = computed(() => route?.query?.name?.trim() || route?.params?.name?.trim() || 'World')
 </script>
 
 <template>
